4d883ca99797c9b98dbdfb60ffda0a590bd1b32b
Chris <bluman325@gmail.com>
2022-03-21 14:20:47 -0500

    added weaons and weapon values

--- Weapons.py
+++ Weapons.py
@@ -1,5 +1,13 @@
-class Weapons:
+class Weapon:
     def __init__(self, name, attack_power):
         self.weapon = name
         self.attack_power = attack_power
         
+
+weapon_1 = Weapon(("graviton beam") 30)
+print(weapon_1.weapon_name)
+
+weapon_2 = Weapon(("power axe") 10)
+print(weapon_2.weapon_name)
+
+weapon3 = Weapon(("
