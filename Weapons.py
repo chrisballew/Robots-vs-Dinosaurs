20b6ba02010e395159feb34189a35d743bc58eba
Chris <bluman325@gmail.com>
2022-03-21 13:51:53 -0500

    saved changes

--- Weapons.py
+++ Weapons.py
@@ -1,5 +1,5 @@
 class Weapons:
-    def __init__(self, weapon, attack_power):
-        self.weapon = weapon
-        self.attack_power = attack_power(int)
+    def __init__(self, name, attack_power):
+        self.weapon = name
+        self.attack_power = attack_power
         
